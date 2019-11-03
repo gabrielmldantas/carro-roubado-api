117d70e96be4033271f2510ebf15c852add46e2a
Gabriel Morrison Lima Dantas <gabrielmldantas@gmail.com>
2019-11-03 15:55:53 -0300

    Commit Inicial

---
+++ src/main/java/br/carroroubado/api/PesquisaCarroRoubadoServlet.java
@@ -0,0 +1,19 @@
+package br.carroroubado.api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PesquisaCarroRoubadoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.addHeader("Content-Type", "application/json");
+		resp.getWriter().println("Hello");
+	}
+}
---
+++ src/main/java/br/carroroubado/app/App.java
@@ -0,0 +1,18 @@
+package br.carroroubado.app;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+import br.carroroubado.api.PesquisaCarroRoubadoServlet;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		handler.setContextPath("/carros");
+		server.setHandler(handler);
+		handler.addServlet(PesquisaCarroRoubadoServlet.class, "/pesquisa/*");
+		server.start();
+		server.join();
+	}
+}
