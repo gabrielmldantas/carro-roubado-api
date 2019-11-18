7ed0f38111ea4dd368f7554eacca69dbb96e4156
Gabriel Morrison Lima Dantas <gabriel@infox.com.br>
2019-11-18 10:18:36 -0300

    Formatação

--- src/main/java/br/carroroubado/api/PesquisaCarroRoubadoServlet.java
+++ src/main/java/br/carroroubado/api/PesquisaCarroRoubadoServlet.java
@@ -26,42 +26,42 @@ import software.amazon.awssdk.services.rekognition.model.Image;
 
 public class PesquisaCarroRoubadoServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Gson gson = new Gson();
-		try {
-			resp.addHeader("Content-Type", "application/json");
-			ObjetoRequisicao objetoRequisicao = null;
-			try (Reader reader = new InputStreamReader(req.getInputStream())) {
-				objetoRequisicao = gson.fromJson(reader, ObjetoRequisicao.class);
-			}
-			RekognitionClient client = AwsClientBuilder.buildClient();
-			DetectTextResponse response = client.detectText(DetectTextRequest.builder()
-					.image(Image.builder().bytes(SdkBytes.fromByteArray(objetoRequisicao.getBuffer())).build())
-					.build());
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        try {
+            resp.addHeader("Content-Type", "application/json");
+            ObjetoRequisicao objetoRequisicao = null;
+            try (Reader reader = new InputStreamReader(req.getInputStream())) {
+                objetoRequisicao = gson.fromJson(reader, ObjetoRequisicao.class);
+            }
+            RekognitionClient client = AwsClientBuilder.buildClient();
+            DetectTextResponse response = client.detectText(DetectTextRequest.builder()
+                    .image(Image.builder().bytes(SdkBytes.fromByteArray(objetoRequisicao.getBuffer())).build())
+                    .build());
 
-			Placa placa = Placa.fromRekognitionResponse(response);
+            Placa placa = Placa.fromRekognitionResponse(response);
 
-			try (Connection connection = DatabaseManager.getConnection()) {
-				String sql = "select count(*) from placa where placa = ? and localizacao = ?";
-				try (PreparedStatement ps = connection.prepareStatement(sql)) {
-					ps.setString(1, placa.getNumeracao());
-					ps.setString(2, placa.getLocalizacao());
-					try (ResultSet rs = ps.executeQuery()) {
-						placa.setRoubado(rs.next());
-						System.out.println(gson.toJson(placa));
-						resp.getWriter().print(gson.toJson(placa));
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			JsonObject jsonObject = new JsonObject();
-			jsonObject.add("excecao", new JsonPrimitive(e.getClass().getCanonicalName()));
-			jsonObject.add("mensagem", new JsonPrimitive(e.getMessage() != null ? e.getMessage() : ""));
-			resp.getWriter().print(gson.toJson(jsonObject));
-		}
-	}
+            try (Connection connection = DatabaseManager.getConnection()) {
+                String sql = "select count(*) from placa where placa = ? and localizacao = ?";
+                try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                    ps.setString(1, placa.getNumeracao());
+                    ps.setString(2, placa.getLocalizacao());
+                    try (ResultSet rs = ps.executeQuery()) {
+                        placa.setRoubado(rs.next());
+                        System.out.println(gson.toJson(placa));
+                        resp.getWriter().print(gson.toJson(placa));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.add("excecao", new JsonPrimitive(e.getClass().getCanonicalName()));
+            jsonObject.add("mensagem", new JsonPrimitive(e.getMessage() != null ? e.getMessage() : ""));
+            resp.getWriter().print(gson.toJson(jsonObject));
+        }
+    }
 }
